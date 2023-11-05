40e250c4599dd266f6107a568f53b77c00fecf6c
K1tanaK0mbat <kkeayouth@gmail.com>
2023-11-05 16:12:10 -0600

    completed api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,102 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+try {
+  const CategoryData = await Category.findAll({
+    include: [{model:Product}],
+  attributes: {
+    include: [
+      [
+        sequelize.literal(
+          '(SELECT * FROM product WHERE category.id = product.category_id)'
+        ),
+        'category_name',
+      ],
+    ],
+  },
+  });
+  res.status(200).json(CategoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const CategoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+      attributes: {
+        include: [
+          [
+            sequelize.literal(
+              '(SELECT * FROM product WHERE category.id = product.category_id)'
+            ),
+            'category_name',
+          ],
+        ],
+      },
+    });
+
+    if (!CategoryData) {
+      res.status(404).json({ message: 'That category doesnt exist' });
+      return;
+    }
+
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const CategoryData = await Category.create(req.body);
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try{
+  const CategoryDate = await Category.update({category_name:
+    req.body.category_name,},
+    {where: {
+      id: req.body.id,},
+    }
+  );
+  if (!CategoryDate) {
+    res.status(404).json({ message: 'That category doesnt exist' });
+    return;
+  }
+  res.status(200).json({message:'Changes to category were successful'});
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const CategoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!CategoryData) {
+      res.status(404).json({ message: 'That category doesnt exist' });
+      return;
+    }
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,135 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
-});
+router.get('/', async (req, res) => {
+  try {
+    const Tags = await Tag.findAll({
+      include: [{model:Product}, {model:ProductTag}],
+    attributes: {
+      include: [
+        [
+          sequelize.literal(
+            '(SELECT * FROM product_tag WHERE tag.id = product_tag.tag_id)'
+          ),
+          'tag_id',
+        ],
+      ],
+    },
+    });
+    res.status(200).json(Tags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const Tags = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }, {model:ProductTag}],
+      attributes: {
+        include: [
+          [
+            sequelize.literal(
+              '(SELECT * FROM product_tag WHERE tag.id = product_tag.tag_id)'
+            ),
+            'tag_id',
+          ],
+        ],
+      },
+    });
+
+    if (!Tags) {
+      res.status(404).json({ message: 'Tag id not found' });
+      return;
+    }
+
+    res.status(200).json(Tags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then((tag) => {
+    if (req.body.productIds.length) {
+      const productTagIdArr = req.body.productIds.map((product_id) => {
+        return {
+          tag_id: tag.id,
+          product_id,
+        };
+      });
+      return ProductTag.bulkCreate(productTagIdArr);
+    }
+
+    res.status(200).json(tag);
+  })
+  .then((productTagIds) => res.status(200).json(productTagIds))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => {
+      if (req.body.productIds && req.body.productIds.length) {
+
+        ProductTag.findAll({
+          where: { tag_id: req.params.id }
+        }).then((productTags) => {
+          // create filtered list of new tag_ids
+          const productTagIds = productTags.map(({ product_id }) => product_id);
+          const newProductTags = req.body.productIds
+            .filter((product_id) => !productTagIds.includes(product_id))
+            .map((product_id) => {
+              return {
+                tag_id: req.params.id,
+                product_id,
+              };
+            });
+          const productTagsToRemove = productTags
+            .filter(({ product_id }) => !req.body.productIds.includes(product_id))
+            .map(({ id }) => id);
+       
+          return Promise.all([
+            ProductTag.destroy({ where: { id: productTagsToRemove } }),
+            ProductTag.bulkCreate(newProductTags),
+          ]);
+        });
+      }
+
+      return res.json(tag);
+    })
+    .catch((err) => {
+      // console.log(err);
+      res.status(400).json(err);
+    });
 });
 
-router.delete('/:id', (req, res) => {
+
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const Tags = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!Tags) {
+      res.status(404).json({ message: 'Cannot locate that tag' });
+      return;
+    }
+    res.status(200).json(Tags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
